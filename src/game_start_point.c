47125530b1b77118241b8ab84ebee92e4a0cb6ce
bewrrrie <vadim.leshkov@protonmail.com>
2022-06-04 01:44:57 +0700

    add game entry point lib

---
+++ src/game_start_point.c
@@ -0,0 +1,24 @@
+#include "game_start_point.h"
+
+int run_game()
+{
+    char c = getch();
+    int *field;
+    int *init_state;
+    int upd_ms = DEFAULT_DELAY_MS;
+    int n_generations = 1;
+    int is_correct = initialize_field(&field, &init_state);
+    int pause = 0;
+    if (is_correct)
+        init_env();
+    while (is_correct && c != 'q' && c != 'Q')
+        game_cycle_iter(&c, &is_correct, &upd_ms, &field,
+                        init_state, &n_generations, &pause);
+    if (is_correct)
+    {
+        endwin();
+        free(field);
+        free(init_state);
+    }
+    return is_correct;
+}
---
+++ src/game_start_point.h
@@ -0,0 +1,11 @@
+#ifndef GAME_START_POINT_H
+#define GAME_START_POINT_H
+
+#include <ncurses.h>
+#include <stdlib.h>
+
+#include "game_of_life.h"
+
+int run_game();
+
+#endif
