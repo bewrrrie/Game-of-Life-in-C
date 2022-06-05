2a3c30980aaa95cbcf53a810773a508d96a62f53
bewrrrie <vadim.leshkov@proton.me>
2022-06-05 19:14:13 +0700

    add condition if executable doesn't exist

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
-echo "Compiling executable binary..."
-cd src
-make compile
-cp gol ../
-cd ..
-echo "Compiled executable \"gol\"."
+if [ ! -e gol ]; then
+    echo "Compiling executable binary..."
+    cd src
+    make compile
+    cp gol ../
+    cd ..
+    echo "Compiled executable \"gol\"."
+fi
 
 if [ $# -lt 1 ]; then
     ./gol < samples/gosper_gun.txt
